b6ff0c8e71d86ed78d90d353611e0bd00b4e2f8b
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-29 11:43:38 -0400

    first draft

---
+++ HomeSales.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace HomeSales;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int max = 0;
+        int sumsale = 0;
+        string salesperson = "";
+        int sale = 0;
+        string maxsalesperson = "";
+        for (; ; )
+        {
+            Console.Write("salesperson: ");
+            salesperson = Console.ReadLine();
+            if (salesperson == "z" || salesperson == "Z")
+            {
+                break;
+            }
+            if ((salesperson != "F" && salesperson != "D" && salesperson != "E") && (salesperson != "f" && salesperson != "d" && salesperson != "e"))
+            {
+                Console.WriteLine("intermediate output: Error, invalid salesperson selected, please try again");
+                continue;
+            }
+            Console.Write("sale: ");
+            sale = Convert.ToInt32(Console.ReadLine());
+            sumsale = sumsale + sale;
+            if (sale > max)
+            {
+                max = sale;
+                maxsalesperson = salesperson;
+            }
+        }
+
+        Console.WriteLine("Grand Total: " + sumsale);
+        Console.WriteLine("Highest Sale: " + maxsalesperson);
+
+    }
+}
